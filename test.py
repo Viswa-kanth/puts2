988f928c584af66e597fe030d668b891cd9c3c92
Viswa-kanth <viswakanthreddy06@gmail.com>
2020-08-08 20:43:47 +0200

    working now

--- test.py
+++ test.py
@@ -8,32 +8,32 @@ class TestCalculator(unittest.TestCase):
 		main.app.testing = True
 		self.app = main.app.test_client()
 
-	def test_mean1(self):
+	def test_median1(self):
 
 		solution = self.app.get('/median?X=22.222,98,4,2,1,5,6,77,99,89')
 		self.assertEqual(b'14.111', solution.data)
 
-	def test_mean2(self):
+	def test_median2(self):
 
 		solution = self.app.get('/median?X=1/2,89')
 		self.assertEqual(b'44.75', solution.data)
 
-	def test_mean3(self):
+	def test_median3(self):
 
 		solution = self.app.get('/median?X=30,5/2')
 		self.assertEqual(b'16.25', solution.data)
 
-	def test_avg2(self):
+	def test_median4(self):
 
 		solution = self.app.get('/median?X=22,23,24,25,26,27,28,100')
 		self.assertEqual(b'25.5', solution.data)
 
-	def test_avg3(self):
+	def test_median5(self):
 
 		solution = self.app.get('median?X=1,98')
 		self.assertEqual(b"49.5", solution.data) 
 
-	def test_avg4(self):
+	def test_median6(self):
 
 		solution = self.app.get('median?X=10,2,1,2,3,4,5,6,7,8,9,3,2,1,1,123')
 		self.assertEqual(b"3.5", solution.data)
