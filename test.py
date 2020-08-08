0c02cf90a55e3f69c5833083c785339d1517870a
Viswa-kanth <viswakanthreddy06@gmail.com>
2020-08-08 20:40:07 +0200

    working now

--- test.py
+++ test.py
@@ -8,50 +8,35 @@ class TestCalculator(unittest.TestCase):
 		main.app.testing = True
 		self.app = main.app.test_client()
 
-	def test_median1(self):
-
-		solution = self.app.get('/median?X=1,4,5')
-		self.assertEqual(b'4.6', solution.data)
-
-	def test_median2(self):
-
-		solution = self.app.get('/median?X=0.5,12.23,0.23.100,1,3,12')
-		self.assertEqual(b'21.878888888888888', solution.data)
-
-	def test_median3(self):
+	def test_mean1(self):
 
 		solution = self.app.get('/median?X=22.222,98,4,2,1,5,6,77,99,89')
-		self.assertEqual(b'40.3222', solution.data)
+		self.assertEqual(b'14.111', solution.data)
 
-	def test_median4(self):
+	def test_mean2(self):
 
-		solution = self.app.get('/median?X=1/2,89,23,24')
+		solution = self.app.get('/median?X=1/2,89')
 		self.assertEqual(b'44.75', solution.data)
 
-	def test_median5(self):
+	def test_mean3(self):
 
 		solution = self.app.get('/median?X=30,5/2')
 		self.assertEqual(b'16.25', solution.data)
 
-	def test_median6(self):
+	def test_avg2(self):
 
-		solution = self.app.get('/median?X=0,22,23,4,6,2')
-		self.assertEqual(b'9.571428571428571', solution.data)
+		solution = self.app.get('/median?X=22,23,24,25,26,27,28,100')
+		self.assertEqual(b'25.5', solution.data)
 
-	def test_median7(self):
+	def test_avg3(self):
 
-		solution = self.app.get('/median?X=22,23,24,26,27,28,100')
-		self.assertEqual(b'34.375', solution.data)
-
-	def test_median8(self):
-
-		solution = self.app.get('median?X=1,2,5,0,100')
+		solution = self.app.get('median?X=1,98')
 		self.assertEqual(b"49.5", solution.data) 
 
-	def test_median9(self):
+	def test_avg4(self):
 
-		solution = self.app.get('median?X=10,2,1,2,3,4,5,6,7,2,1,1,123')
-		self.assertEqual(b"11.6875", solution.data)
+		solution = self.app.get('median?X=10,2,1,2,3,4,5,6,7,8,9,3,2,1,1,123')
+		self.assertEqual(b"3.5", solution.data)
 
 if __name__ == '__main__':
 	unittest.main()
