b837e15f311c6bf9e3f8ea6bc0b226398a45cae6
Viswa-kanth <viswakanthreddy06@gmail.com>
2020-08-08 18:58:40 +0200

    added max(value) to get maximum number among the input

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 from flask import Flask, request
+from fractions import Fraction
+import json
+import statistics
+import decimal
+from decimal import Decimal
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
+    #return 'Usage;\nOperation?X=<Value1>\n' api syntax failed for single vector operand
+    if request.method == 'GET':
+        value1 = request.args.get('X', default=0, type=str)
+
+    else:
+        value1 = request.values.get('X', default=0, type=str)
+    try:
+        value1 = [Fraction(value) for value in value1.split(',')]
+    except ValueError:
+        error = "error, enter proper input"
+        return error
+
+    return value1
 
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
-
+@app.route('/max')
+def maximum():
+    try:
+        value1 = index()
+        result = max(value1)
+    except ValueError:
+        error = index()
+        return error
+    else:
+        if float(result).is_integer():#to avoid unsuported operand types
+            answer = int(result)
+            return "%d" % result
+        return str(float(round(result)))
 
 if __name__ == "__main__":
     app.run()
